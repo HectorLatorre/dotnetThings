0e7d675bf65b1d692a388dcdec4715eb78a20850
Hector Latorre <hlatorre@lawlogix.com>
2017-06-16 11:19:54 -0400

    View Showing Users from database, refer to UserController and Index file on View/User

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using firstapp.Data;
 
 namespace firstapp.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using firstapp.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace firstapp.Controllers
+{
+    public class UserController : Controller
+    {
+		private UniContext context;
+		public UserController(UniContext context)
+		{
+			this.context = context;
+		}
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(this.context.Usuarios.ToList());
+        }
+    }
+}
